50d1671024bfa6a417077a62a4acdffbbfe09ec9
Erhan <demirci.erhan@windowslive.com>
2022-01-23 22:18:36 +0300

    first commit

---
+++ gitExample/gitExampleTests/gitExampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  gitExampleTests.swift
+//  gitExampleTests
+//
+//  Created by erhan demirci on 23.01.2022.
+//
+
+import XCTest
+@testable import gitExample
+
+class gitExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
